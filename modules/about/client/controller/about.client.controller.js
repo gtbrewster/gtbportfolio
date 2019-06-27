1999bc8040e74412f379ec031af2843f6a5f4205
gtbrewster <gtbrewster@ufl.edu>
2019-06-27 19:14:07 -0400

    Adding about to the dropdown menu

--- modules/about/client/controller/about.client.controller.js
+++ modules/about/client/controller/about.client.controller.js
@@ -1,13 +1,10 @@
-/**
- * Created by George on 5/1/2017.
- */
 'use strict';
 
 //About controller
 angular.module('about').controller('AboutController', ['$scope', 'Authentication',
   function($scope, Authentication){
     $scope.authentication = Authentication;
-
+    
 
   }
 ]);
